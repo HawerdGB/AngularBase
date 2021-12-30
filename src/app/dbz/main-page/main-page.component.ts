49fb8aff2e29cfee09732ab9dce14842b26ed606
hgutierrez <hgutierrez@OSC0100239>
2021-12-30 12:23:27 -0400

    DBZ module and using ngSubmit

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+
+})
+export class MainPageComponent  {
+
+  nombrePersonaje: string = "";
+  poder: number = 0
+
+  agregar( ):void{
+        
+  }
+
+}
